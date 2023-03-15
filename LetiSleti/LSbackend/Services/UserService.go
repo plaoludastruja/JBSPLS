1ebd71a12a989cd8b0383b111127a0511cb655dd
Đorđe <the2922000@gmail.com>
2023-03-15 20:52:08 +0100

    BE: Password hash

--- LetiSleti/LSbackend/Services/UserService.go
+++ LetiSleti/LSbackend/Services/UserService.go
@@ -3,15 +3,25 @@ package Services
 import (
 	"github.com/plaoludastruja/JBSPLS/LetiSleti/LSbackend/Models"
 	"github.com/plaoludastruja/JBSPLS/LetiSleti/LSbackend/Repository"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func RegisterUser(user Models.User) bool {
-	if Repository.CreateUser(user) {
-		return true
-	}
-	return false
+	HashPassword(&user)
+	return Repository.CreateUser(user)
 }
 
 func GetAllUsers() []Models.User {
 	return Repository.GetAllUsers()
+}
+
+func HashPassword(u *Models.User) error {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(u.Password), 14)
+	u.Password = string(bytes)
+	return nil
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
