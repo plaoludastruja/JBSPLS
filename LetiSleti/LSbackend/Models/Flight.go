55eb91207fece225e0e1fded1464f93135394410
Sandra <sandra.brkic5@gmail.com>
2023-03-16 07:32:14 +0100

    Add model

---
+++ LetiSleti/LSbackend/Models/Flight.go
@@ -0,0 +1,14 @@
+package Models
+
+import (
+	"time"
+)
+
+type Flight struct {
+	Start             time.Time `json:"startTime" bson:"start_time" binding:"required"`
+	StartPlace        string    `json:"startPlace" bson:"start_place" binding:"required"`
+	End               time.Time `json:"endTime" bson:"end_time" binding:"required"`
+	EndPlace          string    `json:"endPlace" bson:"end_place" binding:"required"`
+	MaxNumberOfPlaces int       `json:"maxNumberOfPlaces" bson:"max_number_of_places" binding:"required"`
+	PricePerPlace     int       `json:"pricePErPlace" bson:"price_per_place" binding:"required"`
+}
