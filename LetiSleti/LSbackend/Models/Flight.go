77e21806917db9f6a6ee03363fea95dca65f8714
Sandra <sandra.brkic5@gmail.com>
2023-03-23 14:28:30 +0100

    Fix date format

--- LetiSleti/LSbackend/Models/Flight.go
+++ LetiSleti/LSbackend/Models/Flight.go
@@ -1,14 +1,16 @@
 package Models
 
 import (
+	"time"
+
 	"gopkg.in/mgo.v2/bson"
 )
 
 type Flight struct {
 	Id                bson.ObjectId `json:"id" bson:"_id,omitempty"`
-	Start             string        `json:"start" bson:"start" binding:"required"`
+	Start             time.Time     `json:"start" bson:"start" binding:"required"`
 	StartPlace        string        `json:"startPlace" bson:"startPlace" binding:"required"`
-	End               string        `json:"end"  bson:"end" binding:"required"`
+	End               time.Time     `json:"end"  bson:"end" binding:"required"`
 	EndPlace          string        `json:"endPlace" bson:"endPlace" binding:"required"`
 	MaxNumberOfPlaces int           `json:"maxNumberOfPlaces" bson:"maxNumberOfPlaces" binding:"required"`
 	PricePerPlace     int           `json:"pricePerPlace" bson:"pricePerPlace" binding:"required"`
