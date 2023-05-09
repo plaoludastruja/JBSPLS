35916faface9123050f13538f30ec32e5e6dedaa
Sandra Brkic <sandra.brkic5@gmail.com>
2023-05-09 20:24:49 +0200

    Fix

--- Skitnica/frontend/src/components/register-accomodation/RegisterAccomodation.tsx
+++ Skitnica/frontend/src/components/register-accomodation/RegisterAccomodation.tsx
@@ -5,7 +5,7 @@ import accomodationService from '../../services/accomodation.service';
 
 function RegisterAccomodation() {
 
-    const [accomodation, setAccomodation] = useState<Accomodation>({ Id: '', Name: 'Naziv', Location : '', Facilities : '', MaxNumberOfGuests : 0, MinNumberOfGuests : 0 })
+    const [accomodation, setAccomodation] = useState<Accomodation>({ id: '', name: 'Naziv', location : '', facilities : '', maxNumberOfGuests : 0, minNumberOfGuests : 0 })
     
     const createAccomodation = () => {
         console.log(accomodation)
@@ -15,47 +15,47 @@ function RegisterAccomodation() {
       }
     
     return <>
-        <form className="form">
+        <div className="form">
             <h2 className='heading'>New accomodation</h2>
             <div className='form-fields'>
             <div className='field'>
                 <label>
                 Name:
                 <input type="text" name="name" className='input-field'
-                onChange={(e) => setAccomodation(prevState => ({ ...prevState, Name: e.target.value }))}/>
+                onChange={(e) => setAccomodation(prevState => ({ ...prevState, name: e.target.value }))}/>
                 </label>
             </div>
             <div className='field'>
                 <label>
                 Location:
                 <input type="text" name="name" 
-                onChange={(e) => setAccomodation(prevState => ({ ...prevState, Location: e.target.value }))}/>
+                onChange={(e) => setAccomodation(prevState => ({ ...prevState, location: e.target.value }))}/>
                 </label>
             </div>
             <div className='field'>
                 <label>
                 Facilities:
                 <input type="text" name="name" 
-                onChange={(e) => setAccomodation(prevState => ({ ...prevState, Facilities: e.target.value }))}/>
+                onChange={(e) => setAccomodation(prevState => ({ ...prevState, facilities: e.target.value }))}/>
                 </label>
             </div>
             <div className='field'>
                 <label>
                 Minimum number of guests:
                 <input type="number" name="name"   
-                onChange={(e) => setAccomodation(prevState => ({ ...prevState, MinNumberOfGuests: parseInt(e.target.value) }))}/>
+                onChange={(e) => setAccomodation(prevState => ({ ...prevState, minNumberOfGuests: parseInt(e.target.value) }))}/>
                 </label>
             </div>
             <div className='field'>
                 <label>
                 Maximum number of guests:
                 <input type="text" name="name" 
-                onChange={(e) => setAccomodation(prevState => ({ ...prevState, MaxNumberOfGuests: parseInt(e.target.value) }))}/>
+                onChange={(e) => setAccomodation(prevState => ({ ...prevState, maxNumberOfGuests: parseInt(e.target.value) }))}/>
                 </label>
             </div>
             <div className='field'>
             <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-            //e.stopPropagation()
+            e.stopPropagation()
             createAccomodation()
           }}
           >Create
@@ -64,7 +64,7 @@ function RegisterAccomodation() {
             </div>
             
             
-        </form>
+        </div>
     </>;
   }
 export default RegisterAccomodation;
--- Skitnica/frontend/src/model/Accomodation.ts
+++ Skitnica/frontend/src/model/Accomodation.ts
@@ -1,8 +1,8 @@
 export default interface Accomodation{
-    Id : string;
-    Name: string;
-    Location: string;
-    Facilities: string;
-    MinNumberOfGuests: number;
-    MaxNumberOfGuests: number;
+    id : string;
+    name: string;
+    location: string;
+    facilities: string;
+    minNumberOfGuests: number;
+    maxNumberOfGuests: number;
 }
