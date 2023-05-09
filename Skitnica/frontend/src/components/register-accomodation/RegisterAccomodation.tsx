b6efd0195b6b3a23b84844e27620a9ee5ef45185
Sandra Brkic <sandra.brkic5@gmail.com>
2023-05-09 19:00:10 +0200

    Create model on front

--- Skitnica/frontend/src/components/register-accomodation/RegisterAccomodation.tsx
+++ Skitnica/frontend/src/components/register-accomodation/RegisterAccomodation.tsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
 import './RegisterAccomodation.css';
+import Accomodation from '../../model/Accomodation';
 
 function RegisterAccomodation() {
+
+    const [accomodation, setAccomodation] = useState<Accomodation>({ Id: '', Name: '', Location : '', Facilities : '', MaxNumberOfGuests : 0, MinNumberOfGuests : 0 })
+    
+    const createAccomodation = () => {
+        /*if(validateInput()) {
+          authorService.createAuthor(author).then(() => {
+            props.closeModal()
+            props.setIsAuthorsChanged(true)
+          })
+            .catch(() => {setErrorMessage('Unable to create new author.')})
+        }*/
+        console.log(accomodation);
+      }
+    
     return <>
         <form className="form">
             <h2 className='heading'>New accomodation</h2>
@@ -8,35 +24,45 @@ function RegisterAccomodation() {
             <div className='field'>
                 <label>
                 Name:
-                <input type="text" name="name" className='input-field'/>
+                <input type="text" name="name" className='input-field'
+                onChange={(e) => setAccomodation(prevState => ({ ...prevState, Name: e.target.value }))}/>
                 </label>
             </div>
             <div className='field'>
                 <label>
                 Location:
-                <input type="text" name="name" />
+                <input type="text" name="name" 
+                onChange={(e) => setAccomodation(prevState => ({ ...prevState, Location: e.target.value }))}/>
                 </label>
             </div>
             <div className='field'>
                 <label>
                 Facilities:
-                <input type="text" name="name" />
+                <input type="text" name="name" 
+                onChange={(e) => setAccomodation(prevState => ({ ...prevState, Facilities: e.target.value }))}/>
                 </label>
             </div>
             <div className='field'>
                 <label>
                 Minimum number of guests:
-                <input type="text" name="name" />
+                <input type="number" name="name"   
+                onChange={(e) => setAccomodation(prevState => ({ ...prevState, MinNumberOfGuests: parseInt(e.target.value) }))}/>
                 </label>
             </div>
             <div className='field'>
                 <label>
                 Maximum number of guests:
-                <input type="text" name="name" />
+                <input type="text" name="name" 
+                onChange={(e) => setAccomodation(prevState => ({ ...prevState, MaxNumberOfGuests: parseInt(e.target.value) }))}/>
                 </label>
             </div>
             <div className='field'>
-                <input type="submit" value="Submit" />
+            <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+            e.stopPropagation()
+            createAccomodation()
+          }}
+          >Create
+          </button>
             </div>
             </div>
             
---
+++ Skitnica/frontend/src/model/Accomodation.ts
@@ -0,0 +1,8 @@
+export default interface Accomodation{
+    Id : string;
+    Name: string;
+    Location: string;
+    Facilities: string;
+    MinNumberOfGuests: number;
+    MaxNumberOfGuests: number;
+}
