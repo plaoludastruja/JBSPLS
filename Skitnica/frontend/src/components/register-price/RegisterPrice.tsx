cef609eed2af17d89ba9bf276a8d771a372b4ee5
Sandra Brkic <sandra.brkic5@gmail.com>
2023-05-10 10:29:07 +0200

    Creation

--- Skitnica/frontend/src/components/register-price/RegisterPrice.tsx
+++ Skitnica/frontend/src/components/register-price/RegisterPrice.tsx
@@ -1,19 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Appointment from '../../model/Appointment';
+import Accomodation from '../../model/Accomodation';
+import accomodationService from '../../services/accomodation.service';
 
 function RegisterPrice() {
 
     const [appointment, setAppointment] = useState<Appointment>({ id: '', accomodationId: '', start : '', end : '', priceType : '', price : 0, status: 'Free' })
-    
+    const [accomodations, setAccomodations] = useState<Accomodation[]>([{ id: '', facilities: '', maxNumberOfGuests: 0, location: '', minNumberOfGuests: 0, name: '' }])
+
+    useEffect(() => {
+        accomodationService.getAccomodations().then((response) => {
+            setAccomodations(Object.values(response.data)) //kad je bez Object.values ne vidi kao array, a ovako fali kljuc neki
+            //console.log('nesto')
+            console.log(accomodations)
+        })
+
+        console.log(Array.isArray(accomodations))
+    }, [accomodations])
+    console.log(accomodations)
+
     const createAppointment = () => {
         console.log(appointment)
-        
-      }
+    }
+    /*
+        <select>
+            {accomodations.map(accomodation => (
+                <option value={accomodation.id}>{accomodation.name}</option>
+            ))}
+        </select>
+    */
     
     return <>
         <div className="form">
             <h2 className='heading'>New accomodation</h2>
             <div className='form-fields'>
+            <div className='field'>
+                <label>
+                Accomodation:
+                </label>
+                <select>
+            {accomodations.map(accomodation => (
+                <option value={accomodation.id}>{accomodation.name}</option>
+            ))}
+        </select>
+            </div>
             <div className='field'>
                 <label>
                 Start:
--- Skitnica/frontend/src/services/accomodation.service.ts
+++ Skitnica/frontend/src/services/accomodation.service.ts
@@ -2,8 +2,9 @@ import Accomodation from "../model/Accomodation";
 import { baseAxios } from "./api.service";
 
 const createAccomodation = (accomodation: Accomodation)  => baseAxios.post('/accomodation', accomodation)
-
+const getAccomodations = async()  => await baseAxios.get<Accomodation[]>('/accomodation')
 
 export default {
-    createAccomodation
+    createAccomodation,
+    getAccomodations
   }
