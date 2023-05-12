4becfdbc02f3ada6a0619fb53694152b0e0cffe6
BrankaKljajic <branka.kljajic00@gmail.com>
2023-05-13 00:36:45 +0200

    Started availability check

--- Skitnica/backend/api_gateway/infrastructure/handler/search_handler.go
+++ Skitnica/backend/api_gateway/infrastructure/handler/search_handler.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/plaoludastruja/JBSPLS/Skitnica/backend/api_gateway/infrastructure/service"
 	accomodation "github.com/plaoludastruja/JBSPLS/Skitnica/backend/common/proto/accomodation_service/generated"
+	appointment "github.com/plaoludastruja/JBSPLS/Skitnica/backend/common/proto/appointment_service/generated"
 	reservation "github.com/plaoludastruja/JBSPLS/Skitnica/backend/common/proto/reservation_service/generated"
 )
 
@@ -102,10 +104,57 @@ func (handler *SearchHandler) Search(w http.ResponseWriter, r *http.Request, pat
 		fmt.Println("Find:", find)
 	}
 
-	/*appointmentClient := service.NewAppointmentClient(handler.appointmentClientAddress)
-	appointments, _ := appointmentClient.Search(context.TODO(), &reservation.SearchRequest{StartDay: startDayString, StartMonth: startMonthString, StartYear: startYearString, EndDay: endDayString, EndMonth: endMonthString, EndYear: endYearString})*/
+	appointmentClient := service.NewAppointmentClient(handler.appointmentClientAddress)
+	appointments, _ := appointmentClient.GetAll(context.TODO(), &appointment.GetAllRequest{})
 
-	response, err := json.Marshal(result)
+	const layout = "2006-01-02"
+
+	startYearInt, _ := strconv.Atoi(startYearString)
+	startMonthInt, _ := strconv.Atoi(startMonthString)
+	startDayInt, _ := strconv.Atoi(startDayString)
+
+	endYearInt, _ := strconv.Atoi(endYearString)
+	endMonthInt, _ := strconv.Atoi(endMonthString)
+	endDayInt, _ := strconv.Atoi(endDayString)
+
+	start := time.Date(startYearInt, time.Month(startMonthInt), startDayInt, 00, 00, 00, 999999999, time.UTC)
+	end := time.Date(endYearInt, time.Month(endMonthInt), endDayInt, 00, 00, 00, 999999999, time.UTC)
+
+	result2 := []accomodation.Accomodation{}
+
+	for _, accomodation := range result {
+		for _, appointment := range appointments.Appointments {
+			if appointment.AccomodationId == accomodation.Id {
+				appointmentStart, _ := time.Parse(layout, appointment.Start)
+				appointmentEnd, _ := time.Parse(layout, appointment.End)
+				if appointmentStart.Before(start) && appointmentEnd.After(end) {
+					result2 = append(result2, accomodation)
+				} else if appointmentStart.Before(start) && appointmentEnd.Before(end) && appointmentEnd.After(start) {
+					for _, app := range appointments.Appointments {
+						if app.AccomodationId == accomodation.Id {
+							appStart, _ := time.Parse(layout, app.Start)
+							appEnd, _ := time.Parse(layout, app.End)
+							if appointmentEnd.Equal(appStart) && appEnd.After(end) {
+								result2 = append(result2, accomodation)
+							}
+						}
+					}
+				} else if appointmentStart.After(start) && appointmentStart.Before(end) && appointmentEnd.After(end) {
+					for _, app2 := range appointments.Appointments {
+						if appointment.AccomodationId == accomodation.Id {
+							app2Start, _ := time.Parse(layout, app2.Start)
+							app2End, _ := time.Parse(layout, app2.End)
+							if appointmentStart.Equal(app2End) && app2Start.Before(appointmentStart) {
+								result2 = append(result2, accomodation)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	response, err := json.Marshal(result2)
 	w.WriteHeader(http.StatusOK)
 	w.Write(response)
 
