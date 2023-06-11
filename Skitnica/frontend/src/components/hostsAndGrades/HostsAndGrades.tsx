2bf7bb51f7c7020b892721d11b51f3de7a286df0
Sandra Brkic <sandra.brkic5@gmail.com>
2023-06-11 19:25:39 +0200

    Show hosts on front

--- Skitnica/frontend/src/components/hostsAndGrades/HostsAndGrades.tsx
+++ Skitnica/frontend/src/components/hostsAndGrades/HostsAndGrades.tsx
@@ -1,3 +1,4 @@
+import { MDBBtn } from "mdb-react-ui-kit";
 import HostMark from "../../model/HostMark";
 import User from "../../model/User";
 import userService from "../../services/user.service";
@@ -17,7 +18,45 @@ function HostsAndGrades() {
           });
           
       }, []);
+    
+      const showGrades = (username: string) => {
+        console.log(username)
+      };
 
-  return <>Hosts grades</>;
+  return <>Hosts grades
+    <div className="row">
+        <div className="col-xl-8">
+          <div className="card mb-4 mt-4">
+            <div className="card-header">All reservations</div>
+            <div className="card-body">
+              <div className="table-responsive">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>First name</th>
+                      <th>Last name</th>
+                      <th>Show grades</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  {hosts.map((host) => (
+                    <tbody key={host.id}>
+                      <tr>
+                        <td>{host.firstName}</td>
+                        <td>{host.lastName}</td>
+                        <td><MDBBtn onClick={() => showGrades(host.username)}>
+                              Show grades
+                            </MDBBtn></td>
+                        
+                      </tr>
+                    </tbody>
+                  ))}
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </>;
 }
 export default HostsAndGrades;
