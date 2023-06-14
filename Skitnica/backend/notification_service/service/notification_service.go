87d5bf41f76c815a453f265d101b04278acce710
Đorđe <the2922000@gmail.com>
2023-06-14 21:08:12 +0200

    BE: nats config

--- Skitnica/backend/notification_service/service/notification_service.go
+++ Skitnica/backend/notification_service/service/notification_service.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"fmt"
+
+	nats "github.com/plaoludastruja/JBSPLS/Skitnica/backend/common/saga/messaging/nats"
 	"github.com/plaoludastruja/JBSPLS/Skitnica/backend/notification_service/domain"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -32,5 +35,8 @@ func (service *NotificationService) GetAll() ([]*domain.Notification, error) {
 }
 
 func (service *NotificationService) Insert(notification domain.Notification) error {
+	sub, _ := nats.NewNATSPublisher("nats", "4222", "ruser", "T0pS3cr3t", notification.Receiver)
+	fmt.Println("natsPublish : ", notification.Message)
+	sub.Publish(notification.Message)
 	return service.store.Insert(&notification)
 }
